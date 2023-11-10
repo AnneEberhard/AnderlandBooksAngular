dc446ce0a6d0639346f15932220c4561dc29cba3
AnneEberhard <anne.eberhard@gmx.net>
2023-11-10 19:35:00 +0100

    added js for extra json files

--- src/app/childrensbooks/childrensbooks.component.ts
+++ src/app/childrensbooks/childrensbooks.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PageService } from '../page-service/page.service';
 import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-childrensbooks',
@@ -9,107 +11,24 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class ChildrensbooksComponent {
 
-  childrensBooks = [
-    {
-      section: 'fairytales',
-      title: 'Märchen',
-      books: [
-        {
-          image: 'assets/img/covers/daemonentraum.jpg',
-          paragraphs: [
-            'Paragraph 1',
-            'Paragraph 2',
-            'Paragraph 3'],
-          formats: {
-            eBook: { title: 'eBook', link: 'eBook Link 1' },
-            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
-            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
-          }
-        },
-      ]
-    },
-    {
-      section: 'fipsNina',
-      title: 'Fips und Nina Geschichten',
-      books: [
-        {
-          image: 'assets/img/covers/daemonentraum.jpg',
-          paragraphs: [
-            'Paragraph 1',
-            'Paragraph 2',
-            'Paragraph 3'],
-          formats: {
-            eBook: { title: 'eBook', link: 'eBook Link 1' },
-            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
-            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
-          }
-        },
-        {
-          image: 'assets/img/covers/daemonentraum.jpg',
-          paragraphs: [
-            'Paragraph 1',
-            'Paragraph 2',
-            'Paragraph 3'],
-          formats: {
-            eBook: { title: 'eBook', link: 'eBook Link 1' },
-            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
-            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
-          }
-        },
-        {
-          image: 'assets/img/covers/daemonentraum.jpg',
-          paragraphs: [
-            'Paragraph 1',
-            'Paragraph 2',
-            'Paragraph 3'],
-          formats: {
-            eBook: { title: 'eBook', link: 'eBook Link 1' },
-            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
-            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
-          }
-        },
-        {
-          image: 'assets/img/covers/daemonentraum.jpg',
-          paragraphs: [
-            'Paragraph 1',
-            'Paragraph 2',
-            'Paragraph 3'],
-          formats: {
-            eBook: { title: 'eBook', link: 'eBook Link 1' },
-            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
-            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
-          }
-        },
-        {
-          image: 'assets/img/covers/daemonentraum.jpg',
-          paragraphs: [
-            'Paragraph 1',
-            'Paragraph 2',
-            'Paragraph 3'],
-          formats: {
-            eBook: { title: 'eBook', link: 'eBook Link 1' },
-            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
-            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
-          }
-        },
-        {
-          image: 'assets/img/covers/daemonentraum.jpg',
-          paragraphs: [
-            'Paragraph 1',
-            'Paragraph 2',
-            'Paragraph 3'],
-          formats: {
-            eBook: { title: 'eBook', link: 'eBook Link 1' },
-            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
-            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
-          }
-        },
-      ]
-    },
+  childrensBooks: any;
 
-  ]
+  constructor(private pageService: PageService, public translate: TranslateService, private http: HttpClient) { }
 
-  constructor(private pageService: PageService, public translate: TranslateService) { }
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  async loadData() {
+    try {
+      const data = await firstValueFrom(this.http.get<any>('assets/jsons/childrensbooks.json'));
+      this.childrensBooks = data.childrensBooks;
+    } catch (error) {
+      console.error('Fehler beim Laden der Daten:', error);
+    }
+  }
+  
+
 
   getBookFormats(book: any): any[] {
     const formats = book.formats || {};
