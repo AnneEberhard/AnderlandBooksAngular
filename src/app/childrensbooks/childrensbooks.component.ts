2e9881d50794b30e1a6f4275e3065c704f01276c
AnneEberhard <anne.eberhard@gmx.net>
2023-11-10 17:26:25 +0100

    uploaded content german books

--- src/app/childrensbooks/childrensbooks.component.ts
+++ src/app/childrensbooks/childrensbooks.component.ts
@@ -8,7 +8,119 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./childrensbooks.component.scss']
 })
 export class ChildrensbooksComponent {
-  constructor(private pageService: PageService, public translate: TranslateService) {  }
+
+  childrensBooks = [
+    {
+      section: 'fairytales',
+      title: 'Märchen',
+      books: [
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+      ]
+    },
+    {
+      section: 'fipsNina',
+      title: 'Fips und Nina Geschichten',
+      books: [
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+      ]
+    },
+
+  ]
+
+  constructor(private pageService: PageService, public translate: TranslateService) { }
+
+  getBookFormats(book: any): any[] {
+    const formats = book.formats || {};
+    return Object.keys(formats).map(format => {
+      const formatData = formats[format];
+      return {
+        label: formatData.title,
+        link: formatData.link
+      };
+    });
+  }
 
   scrollToSection(sectionId: string) {
     this.pageService.scrollToSection(sectionId);
--- src/app/englishbooks/englishbooks.component.ts
+++ src/app/englishbooks/englishbooks.component.ts
@@ -8,8 +8,353 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./englishbooks.component.scss']
 })
 export class EnglishbooksComponent {
-  
-  constructor(private pageService: PageService, public translate: TranslateService) {  }
+
+  englishBooks = [
+    {
+      section: 'historical',
+      title: 'Historical Novels',
+      books: [
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+      ]
+    },
+    {
+      section: 'contemporary',
+      title: 'Contemporary',
+      books: [
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+      ]
+    },
+    {
+      section: 'dystopia',
+      title: 'Dystopia',
+      books: [
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+      ]
+    },
+    {
+      section: 'urbanFantasy',
+      title: 'Urban Fantasy',
+      books: [
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+      ]
+    },
+    {
+      section: 'shortStories',
+      title: 'Short Stories',
+      books: [
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+        {
+          image: 'assets/img/covers/daemonentraum.jpg',
+          paragraphs: [
+            'Paragraph 1',
+            'Paragraph 2',
+            'Paragraph 3'],
+          formats: {
+            eBook: { title: 'eBook', link: 'eBook Link 1' },
+            paperback: { title: 'Taschenbuch', link: 'Taschenbuch Link 2' },
+            hardcover: { title: 'Gebundenes Buch', link: 'Gebundenes Buch Link 3' }
+          }
+        },
+      ]
+    },
+  ]
+
+  constructor(private pageService: PageService, public translate: TranslateService) { }
+
+
+  getBookFormats(book: any): any[] {
+    const formats = book.formats || {};
+    return Object.keys(formats).map(format => {
+      const formatData = formats[format];
+      return {
+        label: formatData.title,
+        link: formatData.link
+      };
+    });
+  }
 
   scrollToSection(sectionId: string) {
     this.pageService.scrollToSection(sectionId);
