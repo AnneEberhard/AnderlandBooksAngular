12a99675a8d8265467c58e44bccabc10c272ed33
AnneEberhard <anne.eberhard@gmx.net>
2023-11-10 11:27:10 +0100

    background image

--- src/app/germanbooks/germanbooks.component.ts
+++ src/app/germanbooks/germanbooks.component.ts
@@ -8,6 +8,36 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./germanbooks.component.scss']
 })
 export class GermanbooksComponent {
+
+// In deiner Komponente oder Service-Datei
+germanBooks = [
+  {
+    section: 'historical',
+    title: 'Historische Geschichten',
+    books: [
+      {
+        image: 'assets/img/covers/cover-Niklas-kindle.jpg',
+        paragraphs: ['Paragraph 1', 'Paragraph 2', 'Paragraph 3'],
+        links: ['Link 1', 'Link 2', 'Link 3']
+      },
+      // Weitere Bücher für die historische Sektion
+    ]
+  },
+  {
+    section: 'fantasy',
+    title: 'Fantasy',
+    books: [
+      {
+        image: 'assets/img/covers/Von-Elfen-und-Woelfen-kindle-1.jpg',
+        paragraphs: ['Paragraph 1', 'Paragraph 2', 'Paragraph 3'],
+        links: ['Link 1', 'Link 2', 'Link 3']
+      },
+      // Weitere Bücher für die Fantasy-Sektion
+    ]
+  }
+  // Weitere Sektionen, wenn benötigt
+];
+
   constructor(private pageService: PageService, public translate: TranslateService) {  }
 
   scrollToSection(sectionId: string) {
