01110488f3df59a1bb9c6a18c4940f0de5f9166a
AnneEberhard <anne.eberhard@gmx.net>
2023-11-12 07:16:52 +0100

    added JSON for spirits personage

--- src/app/bree-nan/bree-nan.component.ts
+++ src/app/bree-nan/bree-nan.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PageService } from '../page-service/page.service';
 import { TranslateService } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-bree-nan',
@@ -8,8 +10,22 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./bree-nan.component.scss']
 })
 export class BreeNanComponent {
-  constructor(private pageService: PageService, public translate: TranslateService) {  }
 
+  constructor(private pageService: PageService, public translate: TranslateService, private http: HttpClient) { }
+characters: any;
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  async loadData() {
+    try {
+      const data = await firstValueFrom(this.http.get<any>('assets/jsons/spiritspersons.json'));
+      this.characters = data.characters;
+    } catch (error) {
+      console.error('Fehler beim Laden der Daten:', error);
+    }
+  }
   scrollToSection(sectionId: string) {
     this.pageService.scrollToSection(sectionId);
   }
