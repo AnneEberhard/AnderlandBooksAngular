6f1a1b37efb0dea26564239075f112c83e399711
AnneEberhard <anne.eberhard@gmx.net>
2024-07-05 19:18:23 +0200

    animation to services

--- src/app/offered-services/offered-services.component.ts
+++ src/app/offered-services/offered-services.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
 import { PageService } from '../shared/services/page-service/page.service';
 import { TranslateService } from '@ngx-translate/core';
+import { slideInFromLeft, slideInFromRight } from '../shared/animations';
 
 @Component({
   selector: 'app-services',
   templateUrl: './offered-services.component.html',
-  styleUrls: ['./offered-services.component.scss']
+  styleUrls: ['./offered-services.component.scss'],
+  animations: [slideInFromLeft, slideInFromRight]
 })
-export class OfferedServicesComponent {
-  constructor(private pageService: PageService, public translate: TranslateService) {  }
+export class OfferedServicesComponent implements AfterContentInit{
+  constructor(public pageService: PageService, public translate: TranslateService) {  }
 
-  scrollToSection(sectionId: string) {
+
+  ngAfterContentInit(): void {
+    this.pageService.clearStates();
+    for (let i = 0; i < 8; i++) {
+      this.pageService.registerElement(`serviceElement${i}`);
+    }
+  
+}
+
+scrollToSection(sectionId: string) {
     this.pageService.scrollToSection(sectionId);
   }
 }
