917fffdea121fd633cab4754aa1c3faec55315ab
AnneEberhard <anne.eberhard@gmx.net>
2026-07-21 16:33:52 +0200

    animations temporary deactivated via animations.ts

--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -1,25 +1,44 @@
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
+
 export const slideInFromLeft = trigger('slideInFromLeft', [
-  state('hidden', style({ transform: 'translateX(-150%)' })),
+  state('hidden', style({ transform: 'translateX(0)' })),
   state('visible', style({ transform: 'translateX(0)' })),
-  transition('hidden => visible', [
-    animate('400ms ease-in')
-  ])
+  transition('hidden => visible', [])
 ]);
 
 export const slideInFromRight = trigger('slideInFromRight', [
-  state('hidden', style({ transform: 'translateX(150%)' })),
+  state('hidden', style({ transform: 'translateX(0)' })),
   state('visible', style({ transform: 'translateX(0)' })),
-  transition('hidden => visible', [
-    animate('400ms ease-in')
-  ])
+  transition('hidden => visible', [])
 ]);
 
-export const fadeIn = trigger('fadeIn', [  // Korrigiert: Triggername ist jetzt 'fadeIn'
-  state('hidden', style({ opacity: 0 })),  // Korrigiert: Benutzt 'opacity' statt 'transform'
-  state('visible', style({ opacity: 1 })), 
-  transition('hidden => visible', [
-    animate('700ms ease-in')
-  ])
+export const fadeIn = trigger('fadeIn', [
+  state('hidden', style({ opacity: 1 })),
+  state('visible', style({ opacity: 1 })),
+  transition('hidden => visible', [])
 ]);
+
+//export const slideInFromLeft = trigger('slideInFromLeft', [
+//  state('hidden', style({ transform: 'translateX(-150%)' })),
+//  state('visible', style({ transform: 'translateX(0)' })),
+//  transition('hidden => visible', [
+//    animate('400ms ease-in')
+//  ])
+//]);
+//
+//export const slideInFromRight = trigger('slideInFromRight', [
+//  state('hidden', style({ transform: 'translateX(150%)' })),
+//  state('visible', style({ transform: 'translateX(0)' })),
+//  transition('hidden => visible', [
+//    animate('400ms ease-in')
+//  ])
+//]);
+//
+//export const fadeIn = trigger('fadeIn', [
+//  state('hidden', style({ opacity: 0 })),
+//  state('visible', style({ opacity: 1 })), 
+//  transition('hidden => visible', [
+//    animate('700ms ease-in')
+//  ])
+//]);
