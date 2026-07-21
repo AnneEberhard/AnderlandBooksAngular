6337f3d745df7f9f9d0462e3a28f42df476f5fcd
AnneEberhard <anne.eberhard@gmx.net>
2026-07-21 20:26:27 +0200

    del console log

--- src/app/authors/authors.component.ts
+++ src/app/authors/authors.component.ts
@@ -60,8 +60,6 @@ private scrollToFragment(fragment: string) {
     try {
       const data = await firstValueFrom(this.http.get<any>('assets/jsons/authors.json'));
       this.authors = data.authors;
-      console.log('start');
-      console.log(this.authors);
       this.registerDynamicElements();
     } catch (error) {
       console.error('Fehler beim Laden der Daten:', error);
